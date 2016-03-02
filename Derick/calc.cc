44834e18781b605f22a30e7d1133c606d069189e
ignatius <demokonge17@gmail.com>
2016-03-02 22:00:11 +0300

    added my folder

---
+++ Derick/calc.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+#include"template.h"
+
+using namespace std;
+
+
+int main(int argc, char* argv[])
+{
+int  sum, a=2, b=4;
+float fSum, floata=2.3 ,floatb=4.3;
+calculator<int> calc;
+sum = calc.add(a,b);
+cout<<" The sum of "<<a<<" and "<<b<<" is "<<sum<<endl;
+calculator<float> fcalc;
+fSum = calc.add(floata, floatb);
+cout<<" The sum of "<<floata<<" and "<<floatb<<" is "<<fSum<<endl;
+cout<<endl;
+return 0;
+}
---
+++ Derick/tst.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+cout<<"hi"<<endl;
+cout<<"hello world"<<endl;
+cout<<argc<<endl;
+cout<<argv[0]<<endl<<argv[1]<<endl;
+return 0;
+}
